4c8545fdb9bec64c4c3441339709922f634d9273
bigyan-kc <bigyan.kc1@gmail.com>
2026-08-09 11:09:00 -0400

    Fixing terraform formatting.

--- outputs.tf
+++ outputs.tf
@@ -9,17 +9,17 @@ output "subnet_name" {
 }
 
 output "instance_names" {
-  value = [for instance in google_compute_instance.nodes : instance.name]
+  value       = [for instance in google_compute_instance.nodes : instance.name]
   description = "Names of the created Ubuntu VM instances."
 }
 
 output "instance_public_ips" {
-  value = [for instance in google_compute_instance.nodes : instance.network_interface[0].access_config[0].nat_ip]
+  value       = [for instance in google_compute_instance.nodes : instance.network_interface[0].access_config[0].nat_ip]
   description = "Public IP addresses of the created VM instances."
 }
 
 output "instance_internal_ips" {
-  value = [for instance in google_compute_instance.nodes : instance.network_interface[0].network_ip]
+  value       = [for instance in google_compute_instance.nodes : instance.network_interface[0].network_ip]
   description = "Internal IP addresses of the created VM instances."
 }
 
