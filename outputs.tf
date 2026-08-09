11146c25b030ea2ba55502848ef9a42d9ef4a15d
bigyan-kc <bigyan.kc1@gmail.com>
2026-08-09 16:51:19 -0400

    Fixing unused variable.

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "network_name" {
-  value       = google_compute_network.default_vpc.name
-  description = "Name of the created VPC network."
-}
-
-output "subnet_name" {
-  value       = google_compute_subnetwork.default_subnet.name
-  description = "Name of the created subnet."
-}
-
 output "master_instance_names" {
   value       = [google_compute_instance.master.name]
   description = "Names of the created master VM instance."
